0a396975b2f94adedb49d9e2b24664e42cd70b4b
mattia <mattia.rampazzo@studenti.unitn.it>
2023-04-27 18:40:31 +0200

    get entity added

--- entities.py
+++ entities.py
@@ -1,18 +1,39 @@
 from flask import Blueprint, jsonify, request
-entities_bp = Blueprint('entities', __name__)
-
 from py2neo import Node
-
-
+from py2neo.matching import NodeMatcher
 from neo4jconnector import Client
+
+
+entities_bp = Blueprint('entities', __name__)
 client = Client()
 
 
 # Get an entity
 @entities_bp.route('/<string:e_id>/', methods=['GET'])
 def get_entities(doc_id, e_id):
+    # get the graph db
+    graph_db = client.get_db(doc_id)
+    # match the node
+    nodes = NodeMatcher(graph_db)
+    node = nodes.match("Entity", id=e_id).first() 
+    # otherwise evaluate with Cypher
 
-    return jsonify({'doc_id': doc_id, 'e_id': e_id})
+    # construct the return JSON
+    element_id = ""
+    element_props = {}
+    for key in node.keys():
+        if(key=='id'):
+            element_id = node[key]
+        else:
+            element_props[key] = node[key]
+
+    element = {
+        "entity" : {
+            element_id : element_props
+        }
+    }
+
+    return jsonify(element)
 
 
 # Create an entity
@@ -31,8 +52,12 @@ def create_entities(doc_id):
 
     # save in the graph
     graph_db = client.get_db(doc_id)
+    '''
     tx = graph_db.begin()
     tx.create(node)
-    tx.commit()
+    tx.commit() # deprecated
+    '''
+    # print(graph_db.exists(node)) # output False
+    graph_db.create(node)
     
     return "New entity created", 201
