61bb2551983fcf5904b9a55aa77e55a14834afd6
Jclock <41525088+jclock98@users.noreply.github.com>
2023-11-23 16:08:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,11 +18,11 @@ app = Flask(__name__)
 # load the endpoints
 # PRE = "/api/v0/"
 app.register_blueprint(documents_bp)  # url_prefix=PRE+'documents')
-# app.register_blueprint(elements_bp)  # url_prefix=PRE+'documents/<string:doc_id>/elements')
-# app.register_blueprint(entities_bp)  # url_prefix=PRE+'documents/<string:doc_id>/entities')
+app.register_blueprint(elements_bp)  # url_prefix=PRE+'documents/<string:doc_id>/elements')
+app.register_blueprint(entities_bp)  # url_prefix=PRE+'documents/<string:doc_id>/entities')
 app.register_blueprint(activities_bp)  # url_prefix=PRE+'documents/<string:doc_id>/activities')
 app.register_blueprint(agents_bp)  # url_prefix=PRE+'documents/<string:doc_id>/agents')
-# app.register_blueprint(relations_bp)  # url_prefix=PRE+'documents/<string:doc_id>/relations')
+app.register_blueprint(relations_bp)  # url_prefix=PRE+'documents/<string:doc_id>/relations')
 
 # connect to neo4j service
 neo4j.connect()
