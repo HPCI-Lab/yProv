d0d455b641e81bd761f7539a7ffe23ed4f3587b2
Jclock <jacopo.clocchiatti@gmail.com>
2023-12-21 14:43:36 +0100

    neo4j from process to deamon

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 sed -i '/server.default_listen_address=0.0.0.0/s/^#//' /etc/neo4j/neo4j.conf
 
 ./../usr/bin/neo4j-admin dbms set-initial-password $PASSWORD
-./../usr/bin/neo4j console&
+./../usr/bin/neo4j start
 
 status_code=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:7474/)
 while [ $status_code -ne 200 ]; do
