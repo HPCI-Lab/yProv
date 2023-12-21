23f6be7ce54cb3a64eae94038af10025696e721d
Fabrizio Antonio <fabrizio.antonio@cmcc.it>
2023-12-21 12:55:11 +0100

    Enable server.default_listen_address in Neo4J conf

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sed -i '/server.default_listen_address=0.0.0.0/s/^#//' /etc/neo4j/neo4j.conf
+
 ./../usr/bin/neo4j-admin dbms set-initial-password $PASSWORD
 ./../usr/bin/neo4j console&
 
